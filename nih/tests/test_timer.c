ec3da3fe8d6f558026a33f6bd3264bb771bb0fb6
Scott James Remnant <scott@netsplit.com>
2006-12-07 12:57:11 +0000

    * nih/tests/test_timer.c: Port to the new test framework.

--- nih/tests/test_timer.c
+++ nih/tests/test_timer.c
@@ -19,14 +19,12 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
  */
 
-#include <config.h>
-
+#include <nih/test.h>
 
 #include <time.h>
-#include <stdio.h>
 #include <string.h>
 
-#include <nih/alloc.h>
+#include <nih/macros.h>
 #include <nih/list.h>
 #include <nih/timer.h>
 
@@ -43,255 +41,133 @@ my_callback (void *data, NihTimer *timer)
 	last_timer = timer;
 }
 
-int
+void
 test_add_timeout (void)
 {
-	NihTimer *timer, *ptr;
+	NihTimer *timer;
 	time_t    t1, t2;
-	int       ret = 0;
 
-	printf ("Testing nih_timer_add_timeout()\n");
+	/* Check that we can add a timeout function and that the structure
+	 * returned is correctly populated and placed in the timers list.
+	 */
+	TEST_FUNCTION ("nih_timer_add_timeout");
 	t1 = time (NULL);
-	timer = nih_timer_add_timeout (NULL, 10, my_callback, &t1);
+	timer = nih_timer_add_timeout (NULL, 10, my_callback, &timer);
 	t2 = time (NULL);
 
-	/* Timer should be a timeout one */
-	if (timer->type != NIH_TIMER_TIMEOUT) {
-		printf ("BAD: type set incorrectly.\n");
-		ret = 1;
-	}
-
-	/* Due time should be in 10s time */
-	if ((timer->due < t1 + 10) || (timer->due > t2 + 10)) {
-		printf ("BAD: due time set incorrectly.\n");
-		ret = 1;
-	}
-
-	/* Timeout should be set to that given */
-	if (timer->timeout != 10) {
-		printf ("BAD: timeout set incorrectly.\n");
-		ret = 1;
-	}
-
-	/* Callback should be callback given */
-	if (timer->callback != my_callback) {
-		printf ("BAD: callback set incorrectly.\n");
-		ret = 1;
-	}
-
-	/* Callback data should be pointer given */
-	if (timer->data != &t1) {
-		printf ("BAD: callback data set incorrectly.\n");
-		ret = 1;
-	}
-
-	/* Should be in the timer list */
-	if (NIH_LIST_EMPTY (&timer->entry)) {
-		printf ("BAD: not placed into timers list.\n");
-		ret = 1;
-	}
-
-	/* Should have been allocated using nih_alloc */
-	if (nih_alloc_size (timer) != sizeof (NihTimer)) {
-		printf ("BAD: nih_alloc was not used.\n");
-		ret = 1;
-	}
+	TEST_ALLOC_SIZE (timer, sizeof (NihTimer));
+	TEST_LIST_NOT_EMPTY (&timer->entry);
+	TEST_EQ (timer->type, NIH_TIMER_TIMEOUT);
+	TEST_GE (timer->due, t1 + 10);
+	TEST_LE (timer->due, t2 + 10);
+	TEST_EQ (timer->timeout, 10);
+	TEST_EQ_P (timer->callback, my_callback);
+	TEST_EQ_P (timer->data, &timer);
 
 
-	/* Timer should be next due */
-	ptr = nih_timer_next_due ();
-	if (ptr != timer) {
-		printf ("BAD: timer not next due.\n");
-		ret = 1;
-	}
+	/* Check that the timer is the next one due. */
+	TEST_EQ_P (nih_timer_next_due (), timer);
+
 
 	nih_list_free (&timer->entry);
-
-	return ret;
 }
 
-int
+void
 test_add_periodic (void)
 {
-	NihTimer *timer, *ptr;
+	NihTimer *timer;
 	time_t    t1, t2;
-	int       ret = 0;
 
-	printf ("Testing nih_timer_add_periodic()\n");
+	/* Check that we can add a periodic function and that the structure
+	 * returned is correctly populated and placed in the timers list.
+	 */
+	TEST_FUNCTION ("nih_timer_add_periodic");
 	t1 = time (NULL);
-	timer = nih_timer_add_periodic (NULL, 25, my_callback, &t1);
+	timer = nih_timer_add_periodic (NULL, 25, my_callback, &timer);
 	t2 = time (NULL);
 
-	/* Timer should be a periodic one */
-	if (timer->type != NIH_TIMER_PERIODIC) {
-		printf ("BAD: type set incorrectly.\n");
-		ret = 1;
-	}
-
-	/* Due time should be in 25s time */
-	if ((timer->due < t1 + 25) || (timer->due > t2 + 25)) {
-		printf ("BAD: due time set incorrectly.\n");
-		ret = 1;
-	}
-
-	/* Period should be that given */
-	if (timer->period != 25) {
-		printf ("BAD: period set incorrectly.\n");
-		ret = 1;
-	}
-
-	/* Callback should be callback given */
-	if (timer->callback != my_callback) {
-		printf ("BAD: callback set incorrectly.\n");
-		ret = 1;
-	}
-
-	/* Callback data should be pointer given */
-	if (timer->data != &t1) {
-		printf ("BAD: callback data set incorrectly.\n");
-		ret = 1;
-	}
-
-	/* Should be in the timer list */
-	if (NIH_LIST_EMPTY (&timer->entry)) {
-		printf ("BAD: not placed into timers list.\n");
-		ret = 1;
-	}
-
-	/* Should have been allocated using nih_alloc */
-	if (nih_alloc_size (timer) != sizeof (NihTimer)) {
-		printf ("BAD: nih_alloc was not used.\n");
-		ret = 1;
-	}
+	TEST_ALLOC_SIZE (timer, sizeof (NihTimer));
+	TEST_LIST_NOT_EMPTY (&timer->entry);
+	TEST_EQ (timer->type, NIH_TIMER_PERIODIC);
+	TEST_GE (timer->due, t1 + 25);
+	TEST_LE (timer->due, t2 + 25);
+	TEST_EQ (timer->timeout, 25);
+	TEST_EQ_P (timer->callback, my_callback);
+	TEST_EQ_P (timer->data, &timer);
 
 
-	/* Timer should be next due */
-	ptr = nih_timer_next_due ();
-	if (ptr != timer) {
-		printf ("BAD: timer not next due.\n");
-		ret = 1;
-	}
+	/* Check that the timer is the next one due. */
+	TEST_EQ_P (nih_timer_next_due (), timer);
+
 
 	nih_list_free (&timer->entry);
-
-	return ret;
 }
 
-int
+void
 test_add_scheduled (void)
 {
-	NihTimer         *timer, *ptr;
+	NihTimer         *timer;
 	NihTimerSchedule  schedule;
 	time_t            t1, t2;
-	int               ret = 0;
+
+	/* Check that we can add a scheduled timer and that the structure
+	 * returned is correctly populated, including copying the schedule
+	 * from the structure we gave and placed in the timers list.
+	 */
+	TEST_FUNCTION ("nih_timer_add_scheduled");
 
 	memset (&schedule, 0, sizeof (NihTimerSchedule));
 
-	printf ("Testing nih_timer_add_scheduled()\n");
 	t1 = time (NULL);
-	timer = nih_timer_add_scheduled (NULL, &schedule, my_callback, &t1);
+	timer = nih_timer_add_scheduled (NULL, &schedule, my_callback, &timer);
 	t2 = time (NULL);
 
-	/* Timer should be a scheduled one */
-	if (timer->type != NIH_TIMER_SCHEDULED) {
-		printf ("BAD: type set incorrectly.\n");
-		ret = 1;
-	}
-
-	/* FIXME check due time when it's set to something sensible */
-
-	/* Schedule should be a copy of the one we gave */
-	if ((timer->schedule.minutes != schedule.minutes)
-	    || (timer->schedule.hours != schedule.hours)
-	    || (timer->schedule.mdays != schedule.mdays)
-	    || (timer->schedule.months != schedule.months)
-	    || (timer->schedule.wdays != schedule.wdays)) {
-		printf ("BAD: schedule set incorrectly.\n");
-		ret = 1;
-	}
-
-	/* Callback should be callback given */
-	if (timer->callback != my_callback) {
-		printf ("BAD: callback set incorrectly.\n");
-		ret = 1;
-	}
-
-	/* Callback data should be pointer given */
-	if (timer->data != &t1) {
-		printf ("BAD: callback data set incorrectly.\n");
-		ret = 1;
-	}
-
-	/* Should be in the timer list */
-	if (NIH_LIST_EMPTY (&timer->entry)) {
-		printf ("BAD: not placed into timers list.\n");
-		ret = 1;
-	}
-
-	/* Should have been allocated using nih_alloc */
-	if (nih_alloc_size (timer) != sizeof (NihTimer)) {
-		printf ("BAD: nih_alloc was not used.\n");
-		ret = 1;
-	}
+	TEST_ALLOC_SIZE (timer, sizeof (NihTimer));
+	TEST_LIST_NOT_EMPTY (&timer->entry);
+	TEST_EQ (timer->type, NIH_TIMER_SCHEDULED);
+	TEST_EQ (timer->schedule.minutes, schedule.minutes);
+	TEST_EQ (timer->schedule.hours, schedule.hours);
+	TEST_EQ (timer->schedule.mdays, schedule.mdays);
+	TEST_EQ (timer->schedule.months, schedule.months);
+	TEST_EQ (timer->schedule.wdays, schedule.wdays);
+	TEST_EQ_P (timer->callback, my_callback);
+	TEST_EQ_P (timer->data, &timer);
 
 
-	/* Timer should be next due */
-	ptr = nih_timer_next_due ();
-	if (ptr != timer) {
-		printf ("BAD: timer not next due.\n");
-		ret = 1;
-	}
+	/* Check that the timer is the next one due. */
+	TEST_EQ_P (nih_timer_next_due (), timer);
+
 
 	nih_list_free (&timer->entry);
-
-	return ret;
 }
 
 
-int
+void
 test_next_due (void)
 {
-	NihTimer *timer1, *timer2, *timer3, *ptr;
-	int       ret = 0;
+	NihTimer *timer1, *timer2, *timer3;
 
-	printf ("Testing nih_timer_next_due()\n");
-	timer1 = nih_timer_add_timeout (NULL, 10, my_callback, &ret);
-	timer2 = nih_timer_add_timeout (NULL, 5, my_callback, &ret);
-	timer3 = nih_timer_add_timeout (NULL, 15, my_callback, &ret);
+	/* Check that timers become due in the correct order by scheduling
+	 * three in a random order, and then iterating through until there
+	 * are no more left.
+	 */
+	TEST_FUNCTION ("nih_timer_next_due");
+	timer1 = nih_timer_add_timeout (NULL, 10, my_callback, &timer1);
+	timer2 = nih_timer_add_timeout (NULL, 5, my_callback, &timer2);
+	timer3 = nih_timer_add_timeout (NULL, 15, my_callback, &timer3);
 
-	/* First timer due should be the second timer */
-	ptr = nih_timer_next_due ();
-	if (ptr != timer2) {
-		printf ("BAD: return value wasn't what we expected.\n");
-		ret = 1;
-	}
-	nih_list_free (&ptr->entry);
+	TEST_EQ_P (nih_timer_next_due (), timer2);
+	nih_list_free (&timer2->entry);
 
-	/* Second timer due should be the first timer */
-	ptr = nih_timer_next_due ();
-	if (ptr != timer1) {
-		printf ("BAD: return value wasn't what we expected.\n");
-		ret = 1;
-	}
-	nih_list_free (&ptr->entry);
+	TEST_EQ_P (nih_timer_next_due (), timer1);
+	nih_list_free (&timer1->entry);
 
-	/* Third timer due should be the third timer */
-	ptr = nih_timer_next_due ();
-	if (ptr != timer3) {
-		printf ("BAD: return value wasn't what we expected.\n");
-		ret = 1;
-	}
-	nih_list_free (&ptr->entry);
+	TEST_EQ_P (nih_timer_next_due (), timer3);
+	nih_list_free (&timer3->entry);
 
-	/* No more timers should be due */
-	ptr = nih_timer_next_due ();
-	if (ptr != NULL) {
-		printf ("BAD: return value wasn't what we expected.\n");
-		ret = 1;
-	}
-
-	return ret;
+	TEST_EQ_P (nih_timer_next_due (), NULL);
 }
+
 
 static int destroyed = 0;
 
@@ -303,20 +179,23 @@ my_destructor (void *ptr)
 	return 0;
 }
 
-int
+void
 test_poll (void)
 {
 	NihTimer *timer1, *timer2;
 	time_t    t1, t2;
-	int       ret = 0;
 
-	printf ("Testing nih_timer_poll()\n");
-	timer1 = nih_timer_add_timeout (NULL, 10, my_callback, &t1);
-	timer2 = nih_timer_add_periodic (NULL, 20, my_callback, &t2);
+	TEST_FUNCTION ("nih_timer_poll");
+	timer1 = nih_timer_add_timeout (NULL, 10, my_callback, &timer1);
+	timer2 = nih_timer_add_periodic (NULL, 20, my_callback, &timer2);
 
 	nih_alloc_set_destructor (timer1, my_destructor);
 	nih_alloc_set_destructor (timer2, my_destructor);
 
+	/* Check that we can poll for timers to be triggered, and have the
+	 * first timeout run.  Once run, the timer should be destroyed.
+	 */
+	TEST_FEATURE ("...with timeout");
 	callback_called = 0;
 	last_data = NULL;
 	last_timer = NULL;
@@ -325,30 +204,16 @@ test_poll (void)
 	timer1->due = time (NULL) - 5;
 	nih_timer_poll ();
 
-	/* Only one timer should have been triggered */
-	if (callback_called != 1) {
-		printf ("BAD: incorrect number of timers called.\n");
-		ret = 1;
-	}
+	TEST_EQ (callback_called, 1);
+	TEST_EQ_P (last_timer, timer1);
+	TEST_EQ_P (last_data, &timer1);
+	TEST_TRUE (destroyed);
 
-	/* Timer should have been the first one */
-	if (last_timer != timer1) {
-		printf ("BAD: last timer called wasn't what we expected.\n");
-		ret = 1;
-	}
 
-	/* Data should have been the data given to that timer */
-	if (last_data != &t1) {
-		printf ("BAD: last data wasn't what we expected.\n");
-		ret = 1;
-	}
-
-	/* Timer should have been destroyed */
-	if (! destroyed) {
-		printf ("BAD: timer wasn't destroyed.\n");
-		ret = 1;
-	}
-
+	/* Check that we can poll again and have the periodic timer run,
+	 * that should be rescheduled instead of destroyed.
+	 */
+	TEST_FEATURE ("...with periodic timer");
 	callback_called = 0;
 	last_data = NULL;
 	last_timer = NULL;
@@ -359,39 +224,15 @@ test_poll (void)
 	nih_timer_poll ();
 	t2 = time (NULL);
 
-	/* Only one timer should have been triggered */
-	if (callback_called != 1) {
-		printf ("BAD: incorrect number of timers called.\n");
-		ret = 1;
-	}
+	TEST_EQ (callback_called, 1);
+	TEST_EQ_P (last_timer, timer2);
+	TEST_EQ_P (last_data, &timer2);
+	TEST_FALSE (destroyed);
+	TEST_GE (timer2->due, t1 + 20);
+	TEST_LE (timer2->due, t2 + 20);
 
-	/* Timer should have been the second one */
-	if (last_timer != timer2) {
-		printf ("BAD: last timer called wasn't what we expected.\n");
-		ret = 1;
-	}
-
-	/* Data should have been the data given to that timer */
-	if (last_data != &t2) {
-		printf ("BAD: last data wasn't what we expected.\n");
-		ret = 1;
-	}
-
-	/* Timer should not have been destroyed */
-	if (destroyed) {
-		printf ("BAD: timer was destroyed.\n");
-		ret = 1;
-	}
-
-	/* Timer due date should have been updated */
-	if ((timer2->due < t1 + 20) || (timer2->due > t2 + 20)) {
-		printf ("BAD: timer due date not updated.\n");
-		ret = 1;
-	}
 
 	nih_list_free (&timer2->entry);
-
-	return ret;
 }
 
 
@@ -399,13 +240,11 @@ int
 main (int   argc,
       char *argv[])
 {
-	int ret = 0;
+	test_add_timeout ();
+	test_add_periodic ();
+	test_add_scheduled ();
+	test_next_due ();
+	test_poll ();
 
-	ret |= test_add_timeout ();
-	ret |= test_add_periodic ();
-	ret |= test_add_scheduled ();
-	ret |= test_next_due ();
-	ret |= test_poll ();
-
-	return ret;
+	return 0;
 }

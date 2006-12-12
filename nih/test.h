1a00336a8671b7d7ec6f5e394a91ad86cedbdd49
Scott James Remnant <scott@netsplit.com>
2006-12-12 17:18:11 +0000

    * nih/test.h (TEST_CHILD_WAIT, TEST_CHILD_RELEASE): Sometimes the primitive "when the child is running" locking provided by TEST_CHILD isn't sufficient, and we actually need to perform some actions in the child before we let the parent carry on.  These two macros allow that.

--- nih/test.h
+++ nih/test.h
@@ -313,6 +313,55 @@
 			} else
 
 /**
+ * TEST_CHILD_WAIT:
+ * @_pid: variable to store pid in,
+ * @_fd: variable to store lock fd in.
+ *
+ * Spawn a child in which a test can be performed while the parent waits
+ * in this macro for the child to reach the TEST_CHILD_RELEASE macro
+ * or die.
+ *
+ * The pid of the child is stored in @_pid, a file descriptor is stored
+ * in the @_fd variable which is used by the TEST_CHILD_RELEASE macro.
+ *
+ * A block of code should follow this macro, which is the code that will
+ * be run in the child process; if the block ends, the child will abort.
+ **/
+#define TEST_CHILD_WAIT(_pid, _fd) \
+	do { \
+		int _test_fds[2]; \
+		pipe (_test_fds); \
+		_pid = fork (); \
+		if (_pid > 0) { \
+			char _test_buf[1]; \
+			close (_test_fds[1]); \
+			read (_test_fds[0], _test_buf, 1); \
+			close (_test_fds[0]); \
+		} else if (_pid == 0) { \
+			close (_test_fds[0]); \
+			_fd = _test_fds[1]; \
+		} \
+	} while (0); \
+	if (_pid == 0) \
+		for (int _test_child = 0; _test_child < 2; _test_child++) \
+			if (_test_child) { \
+				abort (); \
+			} else
+
+/**
+ * TEST_CHILD_RELEASE:
+ * @_fd: variable lock fd stored in.
+ *
+ * Release the parent of a child spawned by TEST_CHILD_WAIT now that the
+ * child has reached the critical point.
+ **/
+#define TEST_CHILD_RELEASE(_fd) \
+	do { \
+		write ((_fd), "\n", 1); \
+		close (_fd); \
+	} while (0)
+
+/**
  * TEST_DIVERT_STDOUT_FD:
  * @_fd: fd to send standard output to.
  *

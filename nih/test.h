03faca50fe16efb904e9df61b5c7bd1efb57f0a8
Scott James Remnant <scott@netsplit.com>
2008-06-02 16:04:46 +0100

    * nih/test.h (TEST_DBUS, TEST_DBUS_END): Macros to set up a temporary D-Bus session bus for testing purposes.

--- nih/test.h
+++ nih/test.h
@@ -885,4 +885,60 @@ _test_destructor (void *tag)
 				     (_hash), #_hash); \
 	} while (0)
 
+
+/**
+ * TEST_DBUS:
+ * @_pid: variable to store pid in.
+ *
+ * Spawns a D-Bus bus daemon which may be used for testing purposes.  You
+ * may create connections to the daemon with either the system bus or session
+ * bus addresses, which will both contain its address.
+ *
+ * The pid of the daemon process is stored in @_pid.
+ *
+ * The daemon should be killed with TEST_DBUS_END() once you have finished
+ * with it.
+ **/
+#define TEST_DBUS(_pid)							\
+	do {								\
+		int _test_fds[2];					\
+		assert0 (pipe (_test_fds));				\
+		assert ((_pid = fork ()) >= 0);				\
+		if (_pid > 0) {						\
+			char _test_address[128];			\
+			close (_test_fds[1]);				\
+			assert (read (_test_fds[0], _test_address,	\
+				      sizeof (_test_address)) > 0);	\
+			close (_test_fds[0]);				\
+									\
+			while (strrchr (_test_address, '\n'))		\
+				*(strrchr (_test_address, '\n')) = '\0'; \
+									\
+			assert0 (setenv ("DBUS_SYSTEM_BUS_ADDRESS",	\
+					 _test_address, TRUE));		\
+			assert0 (setenv ("DBUS_SESSION_BUS_ADDRESS",	\
+					 _test_address, TRUE));		\
+		} else if (_pid == 0) {					\
+			close (_test_fds[0]);				\
+			assert (dup2 (_test_fds[1], STDOUT_FILENO) >= 0); \
+			assert0 (execlp ("dbus-daemon", "dbus-daemon",	\
+					 "--session", "--print-address", NULL)); \
+			exit (255);					\
+		}							\
+	} while (0)
+
+/**
+ * TEST_DBUS_END:
+ * @_pid: pid of dbus daemon.
+ *
+ * Terminates a dbus daemon that has been used for testing.
+ **/
+#define TEST_DBUS_END(_pid)				\
+	do {						\
+		kill (_pid, SIGTERM);			\
+		waitpid (_pid, NULL, 0);		\
+		unsetenv ("DBUS_SESSION_BUS_ADDRESS");	\
+		unsetenv ("DBUS_SYSTEM_BUS_ADDRESS");	\
+	} while (0)
+
 #endif /* NIH_TEST_H */

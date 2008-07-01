9e43ddd5a53813ab46ec0a481f17046e6c22f242
Scott James Remnant <scott@netsplit.com>
2008-07-01 13:30:24 +0100

    * nih/tests/test_com.netsplit.Nih.Test_proxy.c: Stub file for proxy test cases. * nih/Makefile.am (TESTS): Build the proxy test suite. (test_com_netsplit_Nih_Test_proxy_SOURCES) (test_com_netsplit_Nih_Test_proxy_CFLAGS) (test_com_netsplit_Nih_Test_proxy_LDFLAGS) (test_com_netsplit_Nih_Test_proxy_LDADD): Details for the proxy test suite, which actually includes the object code since it needs to implement objects too

---
+++ nih/tests/test_com.netsplit.Nih.Test_proxy.c
@@ -0,0 +1,38 @@
+/* libnih
+ *
+ * test_com.netsplit.Nih.Test_proxy.c - test suite for auto-generated
+ * proxy bindings.
+ *
+ * Copyright © 2008 Scott James Remnant <scott@netsplit.com>.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+ */
+
+#include <nih/test.h>
+
+#include <dbus/dbus.h>
+
+#include <nih/dbus.h>
+
+#include "com.netsplit.Nih.Test_object.h"
+#include "com.netsplit.Nih.Test_proxy.h"
+
+
+int
+main (int   argc,
+      char *argv[])
+{
+	return 0;
+}

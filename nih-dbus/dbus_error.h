3db5f8e43d95a5f182431765afd8702e7eb92e41
Scott James Remnant <scott@netsplit.com>
2009-04-27 17:12:22 +0100

    * nih-dbus/dbus_error.h: Add documentation string.

--- nih-dbus/dbus_error.h
+++ nih-dbus/dbus_error.h
@@ -20,6 +20,50 @@
 #ifndef NIH_DBUS_ERROR_H
 #define NIH_DBUS_ERROR_H
 
+/**
+ * Errors in libnih are raised by placing an NihError object in a local
+ * variable that can be retrieved using nih_error_get().  Errors in libdbus
+ * are returned on the stack by passing the address of a DBusError object
+ * when calling functions.
+ *
+ * This module allows the two techniques to be bridged.
+ *
+ * When calling a libnih function, or writing a handler called by such
+ * a function, you may use nih_dbus_error_raise() or
+ * nih_dbus_error_raise_printf().  This may be retrieved by nih_error_get(),
+ * and the handler poll function will do so and convert this into a D-Bus
+ * error message if appropriate.
+ *
+ * For example:
+ *
+ *   nih_dbus_error_raise (DBUS_ERROR_INVALID_ARGS, "Expected Int32");
+ *   return -1;
+ *
+ * When calling a libdbus function you should initialise a DBusError with
+ * dbus_error_init() and pass its address to your function call.  Should
+ * an error be returned, you can raise that so it can be retrieved by
+ * nih_error_get() by passing the name and message members to
+ * nih_dbus_error_raise().
+ *
+ * For example:
+ *
+ *   dbus_error_init (&dbus_err);
+ *   if (! dbus_connection_open (address, &dbus_err)) {
+ *     nih_dbus_error_raise (dbus_err.name, dbus_err.message);
+ *     dbus_error_free (&dbus_err);
+ *
+ *     return -1;
+ *   }
+ *
+ * In both cases, the error variable returned by nih_error_get() is not
+ * actually NihError but NihDBusError.  This extends the original structure
+ * to add a "name" member containing the D-Bus error name.  The error
+ * number for all such errors is NIH_DBUS_ERROR.
+ *
+ * The nih_dbus_message_error() function defined in nih-dbus/dbus_message.h
+ * allows you to send either type of error as a D-Bus error message.
+ **/
+
 #include <nih/macros.h>
 #include <nih/error.h>
 
